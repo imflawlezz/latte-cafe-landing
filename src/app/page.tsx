e1a514ee2e880c1db1bc1ca9985588dbb6a9d082
imflawlezz <egorart6@gmail.com>
2025-06-18 20:34:27 +0200

    feat: add "About Us" section, adjust page.tsx layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import {Header} from "@/components/sections/Header";
 import {Hero} from "@/components/sections/Hero";
+import {AboutUs} from "@/components/sections/AboutUs";
 
 export default function Home() {
     return (
         <>
             <Header />
             <Hero/>
+            <AboutUs/>
         </>
     );
 }
---
+++ src/components/sections/AboutUs.tsx
@@ -0,0 +1,18 @@
+export const AboutUs = () => {
+    return (
+        <section className="py-10 sm:py-20 md:py-30 lg:py-56">
+            <div className="px-10 text-left sm:text-center mx-auto max-w-4xl">
+                <h1 className="font-mono font-bold leading-tight text-lg">
+                    About Us
+                </h1>
+                <p className="sm:text-xl md:text-2xl lg:text-3xl mt-2">
+                    Tucked away in the corner of Wellington, <i>Latte Cafe</i> is a
+                    local neighbourhood cafe and bar. Latte is all about doing
+                    what we love, in a city that we love. We keep stocked
+                    with local produce and pastries. Bring your family for a
+                    bite, or meet your friends for a coffee or warm drink.
+                </p>
+            </div>
+        </section>
+    )
+}
