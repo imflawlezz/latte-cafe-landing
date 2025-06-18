543a248ec1f34cb49b92fe8bc6d94db4a47012d7
imflawlezz <egorart6@gmail.com>
2025-06-18 20:13:33 +0200

    feat: add "Hero" section with necessary assets, adjust page.tsx layout

---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+export const Hero = () => {
+    return (
+        <section className="relative w-full sm:px-8 md:px-14 md:pb-9 bg-[var(--primary-background)]">
+            <div className="relative aspect-[2/3] md:aspect-video lg:aspect-auto lg:h-[640px] w-full">
+                <Image
+                    src="/hero.png"
+                    alt="Cafe interior or hero image"
+                    fill
+                    priority
+                    sizes="(max-width: 767px) 100vw, (max-width: 1023px) 90vw, 80vw"
+                    className="object-cover object-center blur-xs sm:blur-none"
+                    quality={90}
+                />
+            </div>
+        </section>
+    )
+}
